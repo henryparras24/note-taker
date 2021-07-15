18f085601514dfb18c02bfd42d8a9ac0e4766cc5
Henry Parras <henry.parras@yahoo.com>
2021-07-14 23:27:27 -0700

    api routes making progress

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,16 +3,29 @@
  const path = require('path');
  const uniqid = require('uniqid');
 // const store = require('../db/store.js');
- 
-let { notes } = require('../db/db.json');
+ let {notes} = require('../db/db.json');
 
 router.get('/notes', (req, res) => {
     
     res.json(notes);
          
- });
+});
 
+router.post('/notes', (req, res) => {
+    req.body.id = uniqid();
+    const note = newNote(req.body , notes);
+    res.json(note)
+});
 
+function newNote(body, arrayNotes) {
+    const note = body;
+    arrayNotes.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify({ notes: arrayNotes }, null, 2)
+    );
+    return note;
+}
 
 
 
