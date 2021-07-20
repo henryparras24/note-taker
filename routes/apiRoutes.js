5f502836b4af5cb22dd32ee36a0f843e62c95f43
Henry Parras <henry.parras@yahoo.com>
2021-07-20 10:08:11 -0700

    create notes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,20 +3,23 @@
  const path = require('path');
  const uniqid = require('uniqid');
 // const store = require('../db/store.js');
- let {notes} = require('../db/db.json');
+ let notes = require('../db/db.json');
 
+// get all notes 
 router.get('/notes', (req, res) => {
-    
+    console.log(notes);
     res.json(notes);
-         
 });
 
+// create new note
 router.post('/notes', (req, res) => {
     req.body.id = uniqid();
     const note = newNote(req.body , notes);
+    console.log(note);
     res.json(note)
 });
 
+// function for creating new note
 function newNote(body, arrayNotes) {
     const note = body;
     arrayNotes.push(note);
@@ -27,6 +30,7 @@ function newNote(body, arrayNotes) {
     return note;
 }
 
+// delete note
 
 
 module.exports = router
